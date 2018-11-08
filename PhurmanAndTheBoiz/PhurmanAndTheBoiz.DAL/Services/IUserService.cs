551e826c10bd7d24aaa5ae6d41e3b5b4e0a2eb01
ANDRES H CARRERA <andreshcar@live.com>
2018-11-07 18:02:33 -0700

    adding a user service interface and also a user object to be the dto over the wire

---
+++ PhurmanAndTheBoiz/PhurmanAndTheBoiz.DAL/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using PhurmanAndTheBoiz.DAL.Models;
+using PhurmanAndTheBoiz.DAL.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhurmanAndTheBoiz.DAL.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}
