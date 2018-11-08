3ee8c7db472c944b66956b2d13b36aa5beb84c73
ANDRES H CARRERA <andreshcar@live.com>
2018-11-07 21:32:14 -0700

    adding some initial code and finally figured out how to use a builder for options

---
+++ PhurmanAndTheBoiz/PhurmanAndTheBoiz.DAL/Models/AutoMapperUserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PhurmanAndTheBoiz.DAL.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhurmanAndTheBoiz.DAL.Models
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserEntity, User>();
+            CreateMap<User, UserEntity>();
+        }
+    }
+}
---
+++ PhurmanAndTheBoiz/PhurmanAndTheBoiz.DAL/Services/Implementations/SqlUserService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhurmanAndTheBoiz.DAL.Models;
+using PhurmanAndTheBoiz.DAL.Services.Implementations.Context;
+using System;
+using System.Collections.Generic;
+
+namespace PhurmanAndTheBoiz.DAL.Services.Implementations
+{
+    public class SqlUserService : IUserService
+    {
+        private readonly DbContextOptions _options;
+        public SqlUserService()
+        {
+            var builder = new DbContextOptionsBuilder<UserContext>();
+            builder.UseSqlServer(@"Data Source=DESKTOP-KAB0VGA\MILOISGREAT;initial catalog=DnDUsers;Integrated Security=True; MultipleActiveResultSets=True");
+            _options = builder.Options;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Create(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user, string password = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WorkWithConnection(Action<UserContext> unitOfWork)
+        {
+            using (var context = new UserContext(_options)){
+                unitOfWork(context);
+            }
+        }
+    }
+}
