8712ae02dd87ee6eea8f5e4e2ba4245bfc6e6742
ANDRES H CARRERA <andreshcar@live.com>
2018-11-09 22:12:12 -0700

    creating the IDnDService and first class to implement it

---
+++ PhurmanAndTheBoiz/PhurmanAndTheBoiz.DAL/Services/IDnDService.cs
@@ -0,0 +1,25 @@
+﻿using PhurmanAndTheBoiz.DAL.Models.JsonData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhurmanAndTheBoiz.DAL.Services
+{
+    public interface IDnDService
+    {
+        IEnumerable<DnDMap> GetAllDnDMaps();
+        IEnumerable<CharacterSheet> GetAllCharacterSheetsForUser(int userId);
+        IEnumerable<DnDMap> GetAllMapsForUser(int userId);
+        IEnumerable<Item> GetAllItems(int itemId);
+        Item GetItemById(int itemId);
+        void UpdateItem(Item updatedItem);
+        void UpdateCharacter(CharacterSheet updatedCharacter);
+        void UpdateMap(DnDMap updatedMap);
+        Item SaveItem(Item newItem);
+        CharacterSheet SaveCharacter(int userId, CharacterSheet newCharacter);
+        DnDMap SaveMap(int userId, DnDMap newMap);
+        void DeleteMap(int userId, int mapId);
+        void DeleteCharacter(int userId, int characterId);
+        void DeleteItem(int itemId);
+    }
+}
