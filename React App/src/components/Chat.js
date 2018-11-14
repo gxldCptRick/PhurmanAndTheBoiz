280e4dfc81e3c8b625b53f360cea59a88189beae
Fermin <ferminsandoval950@hotmail.com>
2018-11-13 23:30:17 -0700

    Added chat  component

--- React App/src/components/Chat.js
+++ React App/src/components/Chat.js
@@ -1,5 +1,57 @@
 import React,{Component} from 'react';
+import '../index.css';
+import { sendMessage, subscribeToChatMessages } from '../rethinkAPI';
 
-export class Chat extends React.Component{
-  
+export default class Chat extends React.Component{
+    state = {
+        chatMessages: [],
+        message: '',
+    };
+
+    constructor(props){
+        super(props);
+
+        subscribeToChatMessages((chatMessage) => {
+            console.log(`${chatMessage} recieved`);
+            this.setState(prevState => ({
+                chatMessages: prevState.chatMessages.concat([chatMessage])
+            }));
+        })
+    }
+
+    handleSendMessage = () =>{
+        sendMessage({ 
+            message: this.state.message
+        });
+    }
+
+    handleTextChange = (event) => {
+        event.persist();
+        this.setState({
+            message: event.target.value            
+        });
+    }
+
+    render(){
+        const chatMessages = this.state.chatMessages.map(chatMessage =>(
+            <div>
+                {chatMessage.message}
+            </div>
+        ));
+        return (
+        <div className="chat-box">
+            <div className="messages-container">
+                {chatMessages}
+            </div>
+
+            <input 
+                type="text"
+                placeholder="Enter message"
+                onChange={this.handleTextChange}
+            />
+            <input type="button" onClick={this.handleSendMessage} value="Send"/>
+        </div>
+
+        )
+    }
 }
