875dbdd550d098b07815e9ecc1e4b4bb7f481752
ANDRES H CARRERA <andreshcar@live.com>
2018-12-02 19:43:01 -0700

    adding the unit tests to express the use of the apiservice

---
+++ React App/src/helpers/ApiService.spec.js
@@ -0,0 +1,60 @@
+import chai from "chai";
+import * as Api from "./ApiService";
+import mocha from "mocha";
+import { LocalStorage } from "node-localstorage";
+global.localStorage = new LocalStorage("./yes");
+const should = chai.should();
+describe("API Functions Correctly", function() {
+  describe("Get Function Works Correctly For Each Resource", function() {
+    it("Response Should not be null for Users", function(done) {
+      Api.GetResource(Api.Resource.Users)
+        .then(response => response.json())
+        .then(() => done())
+        .catch(err => {
+          console.log(err);
+          done(err);
+        });
+    });
+
+    it("Response Should not be null for Items", function(done) {
+      Api.GetResource(Api.Resource.Items)
+        .then(response => response.json())
+        .then(() => done())
+        .catch(err => {
+          console.log(err);
+          done(err);
+        });
+    });
+
+    it("Response Should not be null for Characters", function(done) {
+      Api.GetResource(Api.Resource.Characters)
+        .then(response => response.json())
+        .then(() => done())
+        .catch(err => {
+          console.log(err);
+          done(err);
+        });
+    });
+
+    it("Response Should not be null for Maps", function(done) {
+      Api.GetResource(Api.Resource.Maps)
+        .then(response => response.json())
+        .then(() => done())
+        .catch(err => {
+          console.log(err);
+          done(err);
+        });
+    });
+  });
+
+  describe("Speicial Fuctions Work Correctly", function() {
+    it("Login Should Have Token On Success", function(done) {
+      Api.LoginUser({ username: "The Batman", password: "Milo@Is3Great" })
+        .then(function(json) {
+          should.exist(json.token);
+        })
+        .then(() => done())
+        .catch(err => done(err));
+    });
+  });
+});
