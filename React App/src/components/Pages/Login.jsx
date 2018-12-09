9c3db83712a6bb3c2bca4b1fbe6412abc8c9e665
blindbatman <emlundaj@gmail.com>
2018-12-09 08:42:06 -0700

    game na chnages

--- React App/src/components/Pages/Login.jsx
+++ React App/src/components/Pages/Login.jsx
@@ -49,7 +49,7 @@ export class Login extends Component {
     return <Redirect to={'/Profile'}/>
    }else{
      return(
-       <div className='form-horizontal'>
+       <div className='bod'>
        <h1>Login</h1>
        {
          Object.keys(this.state.error).map(e => <p className="error" key={e}>{this.state.error[e]}</p>)
