cbe9eb35574cafdeb18d7b5b05904e37db6a9a6f
blindbatman <emlundaj@gmail.com>
2018-12-09 10:31:37 -0700

    more cosmetic fixes?

--- React App/src/components/Pages/Account.jsx
+++ React App/src/components/Pages/Account.jsx
@@ -1,71 +1,69 @@
-import React, {Component} from 'react'
-import {RegisterPage} from './RegisterPage'
-import {Login} from './Login'
-import { Route, Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { RegisterPage } from "./RegisterPage";
+import { Login } from "./Login";
+import { Route, Link } from "react-router-dom";
 
-function LoginDisplay(){
+function LoginDisplay() {
   return (
-    <div className='rightOfPage'>
-      <div>
-        <Login/>
-        <Link to="/Login/SignUp">Register</Link>
-      </div>
-    </div>);
-}
-
-function RegisterDisplay(){
-  return (<div>
-    <RegisterPage/>
-    <Link to="/Login/SignIn">Sign In</Link>
-  </div>
+    <div className="rightOfPage">
+        <Login />
+        <Link to="/Login/SignUp">Register</Link>      
+    </div>
   );
 }
 
-export class Account extends Component{
-  constructor(props){
+function RegisterDisplay() {
+  return (
+    <div>
+      <RegisterPage />
+      <Link to="/Login/SignIn">Sign In</Link>
+    </div>
+  );
+}
+
+export class Account extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      loggedIn: false,
+    this.state = {
+      loggedIn: false
     };
 
     this.showPrompts = this.showPrompts.bind(this);
   }
 
-  componentWillMount(){
+  componentWillMount() {
     var thisuser = JSON.parse(localStorage.getItem("user"));
-    if(thisuser === null){
-      this.setState({loggedIn: false})
-    }else{
-      this.setState({loggedIn: true})
+    if (thisuser === null) {
+      this.setState({ loggedIn: false });
+    } else {
+      this.setState({ loggedIn: true });
     }
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem("user");
-    this.setState({loggedIn: false});
+    this.setState({ loggedIn: false });
   }
 
-  showPrompts(){
-    if(this.state.loggedIn){
-      return(
-        <div className='bod'>
+  showPrompts() {
+    if (this.state.loggedIn) {
+      return (
+        <div className="bod">
           <h1>Logout</h1>
           <button onClick={this.logout.bind(this)}>Log Out</button>
         </div>
-      )
-    }else{
-      return(
+      );
+    } else {
+      return (
         <div>
-          <Route exact path="/Login/SignIn" component={LoginDisplay}/>
-          <Route exact path="/Login/SignUp" component={RegisterDisplay}/>
+          <Route exact path="/Login/SignIn" component={LoginDisplay} />
+          <Route exact path="/Login/SignUp" component={RegisterDisplay} />
         </div>
-      )
+      );
     }
   }
 
-  render(){
-    return(
-      <this.showPrompts/>
-    );
+  render() {
+    return <this.showPrompts />;
   }
 }
